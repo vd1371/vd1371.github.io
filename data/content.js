df53ee32df8db2c8209b0a8c9ea34e152b430d40
Vahid Asghari <vd1371@gmail.com>
2025-05-12 05:56:46 +0800

    Add new posts on "Bet Against Beta in HK Market," "Technical Trading Strategies in Crypto," "Asset Pricing: A Tale of Night and Day," and "Combining Low Volatility with Momentum." Updated existing posts with images and enhanced content. Changed icons in the content.js file.

--- data/content.js
+++ data/content.js
@@ -5,40 +5,65 @@
       title: 'Value and Momentum Everywhere',
       link: 'posts/quantio/2025-05-01-value-and-momentum-everywhere.html',
       date: 'May 1, 2025',
-      icon: 'bi-graph-up',
+      icon: 'bi-currency-exchange',
     },
     {
       title: 'Stock Returns After Extreme Loss Events',
       link: 'posts/quantio/2025-05-01-stock-returns-after-shock.html',
       date: 'May 1, 2025',
-      icon: 'bi-bar-chart',
+      icon: 'bi-lightning-charge',
     },
     {
       title: 'Predicting Indices Using ARIMA',
       link: 'posts/quantio/2025-05-02-predicting-indices-using-arima.html',
       date: 'May 2, 2025',
-      icon: 'bi-calculator',
+      icon: 'bi-graph-up-arrow',
     },
     {
       title: 'Revisiting Momentum',
       link: 'posts/quantio/2025-05-03-revisiting-momentum.html',
       date: 'May 3, 2025',
+      icon: 'bi-arrow-repeat',
+    },
+    {
+      title: 'Combining Low Volatility with Momentum',
+      link: 'posts/quantio/2025-05-04-combining-low-volatility-with-momentum.html',
+      date: 'May 4, 2025',
+      icon: 'bi-diagram-3',
+    }, 
+    {
+      title: 'Asset Pricing: A Tale of Night and Day',
+      link: 'posts/quantio/2025-05-04-asset-pricing-a-tale-of-night-and-day.html',
+      date: 'May 4, 2025',
+      icon: 'bi-moon-stars',
+    },
+    {
+      title: 'Bet Against Beta in HK Market',
+      link: 'posts/quantio/2024-03-20-bet_against_beta_HK.html',
+      date: 'May 4, 2025',
+      icon: 'bi-activity',
+    },
+    {
+      title: 'Technical Trading Strategies in Crypto',
+      link: 'posts/quantio/2024-03-20-technical-trading-strategies-in-crypto.html',
+      date: 'May 4, 2025',
       icon: 'bi-graph-up',
     },
+
   ];
 
   const projects = [
     {
       title: 'Quant Strategies',
       link: 'projects/quant-strategies.html',
-      icon: 'bi-code-slash',
+      icon: 'bi-code-square',
       date: 'To be updated',
     },
     {
       title: 'Crypto News Sentiment',
       date: 'May 2, 2025',
       link: 'posts/projects/2025-05-02-crypto-news-sentiment.html',
-      icon: 'bi-tools',
+      icon: 'bi-chat-square-text',
     },
   ];
 
@@ -47,27 +72,27 @@
       // Expediting life cycle cost analysis of infrastructure assets under multiple uncertainties by deep neural networks
       title: 'Estimating Monte Carlo Simulation results with ML',
       link: 'resources/tutorials.html',
-      icon: 'bi-cpu',
+      icon: 'bi-cpu-fill',
     },
     {
       // Machine learning modeling for spectral transient-based leak detection
       // Upscaling complex project-level infrastructure intervention planning to network assets
       title: 'Estimating Optimization Results with ML',
       link: 'resources/books.html',
-      icon: 'bi-journal-text',
+      icon: 'bi-book',
     },
     {
       // An open-source and extensible platform for general infrastructure asset management system
       title: 'An open-source and extensible platform for general infrastructure asset management system',
       link: 'resources/tools.html',
-      icon: 'bi-layers',
+      icon: 'bi-gear-wide-connected',
     },
     {
       // The Network Level Multiagent reinforcement learning for project-level intervention planning under multiple uncertainties
       title:
         'The Network Level Multiagent reinforcement learning for project-level intervention planning under multiple uncertainties',
       link: 'resources/tools.html',
-      icon: 'bi-cpu',
+      icon: 'bi-diagram-2',
     },
   ];
 
