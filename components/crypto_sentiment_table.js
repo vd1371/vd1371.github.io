2677f6c929881a10898083c5d22d86687f2976be
Vahid Asghari <vd1371@gmail.com>
2025-05-21 07:05:04 +0800

    Improved the table

--- components/crypto_sentiment_table.js
+++ components/crypto_sentiment_table.js
@@ -2,6 +2,49 @@
 function createCryptoSentimentTable(containerId) {
   const container = document.getElementById(containerId);
 
+  const base_link = 'https://coinmarketcap.com/currencies/';
+  const TOP_40_COINS = {
+    BTC: base_link + 'bitcoin',
+    ETH: base_link + 'ethereum',
+    USDT: base_link + 'tether',
+    SOL: base_link + 'solana',
+    BNB: base_link + 'binance-coin',
+    USDC: base_link + 'usd-coin',
+    XRP: base_link + 'xrp',
+    DOGE: base_link + 'dogecoin',
+    ADA: base_link + 'cardano',
+    TRX: base_link + 'tron',
+    AVAX: base_link + 'avalanche-2',
+    SHIB: base_link + 'shiba-inu',
+    LINK: base_link + 'chainlink',
+    DOT: base_link + 'polkadot',
+    BCH: base_link + 'bitcoin-cash',
+    NEAR: base_link + 'near',
+    LTC: base_link + 'litecoin',
+    PEPE: base_link + 'pepe',
+    MATIC: base_link + 'matic-network',
+    ICP: base_link + 'internet-computer',
+    KAS: base_link + 'kaspa',
+    APT: base_link + 'aptos',
+    XMR: base_link + 'monero',
+    XLM: base_link + 'stellar',
+    ETC: base_link + 'ethereum-classic',
+    CRO: base_link + 'crypto-com-coin',
+    SUI: base_link + 'sui',
+    FIL: base_link + 'filecoin',
+    OKB: base_link + 'okb',
+    HBAR: base_link + 'hbar',
+    ARB: base_link + 'arbitrum',
+    ATOM: base_link + 'cosmos',
+    IMX: base_link + 'immunefi',
+    RNDR: base_link + 'render-token',
+    VET: base_link + 'vechain',
+    MKR: base_link + 'maker',
+    GRT: base_link + 'the-graph',
+    OP: base_link + 'optimism',
+    INJ: base_link + 'injective-protocol',
+  };
+
   // Show loading state
   container.innerHTML = '<div class="text-center my-4">Loading sentiment data...</div>';
 
@@ -37,6 +80,7 @@ function createCryptoSentimentTable(containerId) {
             uncertainty_score: parseFloat(values[2]) || 0,
             potential_market_impact: parseFloat(values[3]) || 0,
             count: parseInt(values[4]) || 0,
+            link: TOP_40_COINS[values[0]],
           };
           if (!isNaN(entry.sentiment_score)) {
             parsedData.push(entry);
@@ -65,7 +109,9 @@ function createCryptoSentimentTable(containerId) {
                               .map(
                                 coin => `
                                 <tr>
-                                    <td class="text-center fw-bold">${coin.symbol}</td>
+                                    <td class="text-center fw-bold">
+                                      <a href="${coin.link}" target="_blank">${coin.symbol}</a>
+                                    </td>
                                     <td class="text-center ${coin.sentiment_score > 0 ? 'text-success' : coin.sentiment_score < 0 ? 'text-danger' : ''}">
                                         ${parseFloat(coin.sentiment_score).toFixed(2)}
                                     </td>
