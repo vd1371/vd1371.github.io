3de323686c3308f285b69b7907552acdd1bac49c
Vahid Asghari <vd1371@gmail.com>
2025-11-16 22:15:44 +0800

    rarranging the page

--- data/content.js
+++ data/content.js
@@ -2,116 +2,107 @@
 (function () {
   const quantNotebooks = [
     {
-      title: 'Bet Against Beta in HK Market',
-      link: 'posts/quantio/2024-03-20-bet_against_beta_HK.html',
-      date: '',
-      icon: 'bi-activity',
-    },
-    {
-      title: 'Cross-stock Momentum',
-      link: 'posts/quantio/2024-03-20-cross-stock-momentum.html',
-      date: '',
-      icon: 'bi-arrow-up-right-circle',
-    },
-    {
-      title: 'Technical Trading Strategies in Crypto',
-      link: 'posts/quantio/2024-03-20-technical-trading-strategies-in-crypto.html',
-      date: '',
-      icon: 'bi-graph-up',
-    },
-    {
-      title: 'Stock Returns After Extreme Loss Events',
-      link: 'posts/quantio/2025-05-01-stock-returns-after-shock.html',
-      date: '',
-      icon: 'bi-lightning-charge',
-    },
-    {
-      title: 'Value and Momentum Everywhere',
-      link: 'posts/quantio/2025-05-01-value-and-momentum-everywhere.html',
-      date: '',
-      icon: 'bi-currency-exchange',
-    },
-    {
-      title: 'Predicting Indices Using ARIMA',
-      link: 'posts/quantio/2025-05-02-predicting-indices-using-arima.html',
-      date: '',
-      icon: 'bi-graph-up-arrow',
-    },
-    {
-      title: 'Revisiting Momentum',
-      link: 'posts/quantio/2025-05-03-revisiting-momentum.html',
-      date: '',
-      icon: 'bi-arrow-repeat',
-    },
-    {
-      title: 'Asset Pricing: A Tale of Night and Day',
-      link: 'posts/quantio/2025-05-04-asset-pricing-a-tale-of-night-and-day.html',
-      date: '',
-      icon: 'bi-moon-stars',
-    },
-    {
-      title: 'Combining Low Volatility with Momentum',
-      link: 'posts/quantio/2025-05-04-combining-low-volatility-with-momentum.html',
-      date: '',
-      icon: 'bi-diagram-3',
-    }, 
-    {
-      title: 'Polynomial Moving Regression Band',
-      link: 'posts/quantio/2025-05-13-polynomial-moving-regression-band.html',
-      date: '',
-      icon: 'bi-diagram-2',
-    },
-    {
-      title: 'Dynamic Tactical Asset Allocation',
-      link: 'posts/quantio/2025-05-15-dynamic-tactical-asset-allocation.html',
-      date: '',
-      icon: 'bi-pie-chart',
-    },
-    {
-      title: 'MACD-based Signals',
-      link: 'posts/quantio/2025-05-16-MACD-based-signals.html',
-      date: '',
-      icon: 'bi-graph-up',
-    },
-    {
-      title: 'Holt-Winter for Gold and BTC',
-      link: 'posts/quantio/2025-05-19-Holt-Winter-for-Gold-and-BTC.html',
-      date: '',
-      icon: 'bi-lightning-charge',
-    },    
-    {
-      title: 'Stock Returns After Extreme Loss Events',
-      link: 'posts/quantio/2025-05-21-stock-returns-after-extreme-loss-events.html',
-      date: '',
-      icon: 'bi-graph-up-arrow',
-    },
-    {
-      title: 'Statistical arbitrage strategy based on VIX-to-market based signal',
-      link: 'posts/quantio/2025-05-23-statistical-arbitrage-strategy-based-on-VIX-to-market-based-signal.html',
-      date: '',
-      icon: 'bi-bar-chart-line',
-    },
-    {
-      title: 'Stat Arb in Crude Oil Markets',
-      link: 'posts/quantio/2025-05-25-stat-arb-in-crude-Oil-markets.html',
-      date: '',
-      icon: 'bi-droplet',
-    },
-    {
-      title: 'Pair Trading',
-      link: 'posts/quantio/2025-05-26-pair-trading.html',
-      date: '',
-      icon: 'bi-graph-up',
-    },
-
-  ]
-
-  const projects = [
-    {
-      title: "Quant Strategies",
-      link: "projects/quant-strategies.html",
-      icon: "bi-code-square",
+      title: "Bet Against Beta in HK Market",
+      link: "posts/quantio/2024-03-20-bet_against_beta_HK.html",
       date: "",
+      icon: "bi-activity",
+    },
+    {
+      title: "Cross-stock Momentum",
+      link: "posts/quantio/2024-03-20-cross-stock-momentum.html",
+      date: "",
+      icon: "bi-arrow-up-right-circle",
+    },
+    {
+      title: "Technical Trading Strategies in Crypto",
+      link: "posts/quantio/2024-03-20-technical-trading-strategies-in-crypto.html",
+      date: "",
+      icon: "bi-graph-up",
+    },
+    {
+      title: "Stock Returns After Extreme Loss Events",
+      link: "posts/quantio/2025-05-01-stock-returns-after-shock.html",
+      date: "",
+      icon: "bi-lightning-charge",
+    },
+    {
+      title: "Value and Momentum Everywhere",
+      link: "posts/quantio/2025-05-01-value-and-momentum-everywhere.html",
+      date: "",
+      icon: "bi-currency-exchange",
+    },
+    {
+      title: "Predicting Indices Using ARIMA",
+      link: "posts/quantio/2025-05-02-predicting-indices-using-arima.html",
+      date: "",
+      icon: "bi-graph-up-arrow",
+    },
+    {
+      title: "Revisiting Momentum",
+      link: "posts/quantio/2025-05-03-revisiting-momentum.html",
+      date: "",
+      icon: "bi-arrow-repeat",
+    },
+    {
+      title: "Asset Pricing: A Tale of Night and Day",
+      link: "posts/quantio/2025-05-04-asset-pricing-a-tale-of-night-and-day.html",
+      date: "",
+      icon: "bi-moon-stars",
+    },
+    {
+      title: "Combining Low Volatility with Momentum",
+      link: "posts/quantio/2025-05-04-combining-low-volatility-with-momentum.html",
+      date: "",
+      icon: "bi-diagram-3",
+    },
+    {
+      title: "Polynomial Moving Regression Band",
+      link: "posts/quantio/2025-05-13-polynomial-moving-regression-band.html",
+      date: "",
+      icon: "bi-diagram-2",
+    },
+    {
+      title: "Dynamic Tactical Asset Allocation",
+      link: "posts/quantio/2025-05-15-dynamic-tactical-asset-allocation.html",
+      date: "",
+      icon: "bi-pie-chart",
+    },
+    {
+      title: "MACD-based Signals",
+      link: "posts/quantio/2025-05-16-MACD-based-signals.html",
+      date: "",
+      icon: "bi-graph-up",
+    },
+    {
+      title: "Holt-Winter for Gold and BTC",
+      link: "posts/quantio/2025-05-19-Holt-Winter-for-Gold-and-BTC.html",
+      date: "",
+      icon: "bi-lightning-charge",
+    },
+    {
+      title: "Stock Returns After Extreme Loss Events",
+      link: "posts/quantio/2025-05-21-stock-returns-after-extreme-loss-events.html",
+      date: "",
+      icon: "bi-graph-up-arrow",
+    },
+    {
+      title:
+        "Statistical arbitrage strategy based on VIX-to-market based signal",
+      link: "posts/quantio/2025-05-23-statistical-arbitrage-strategy-based-on-VIX-to-market-based-signal.html",
+      date: "",
+      icon: "bi-bar-chart-line",
+    },
+    {
+      title: "Stat Arb in Crude Oil Markets",
+      link: "posts/quantio/2025-05-25-stat-arb-in-crude-Oil-markets.html",
+      date: "",
+      icon: "bi-droplet",
+    },
+    {
+      title: "Pair Trading",
+      link: "posts/quantio/2025-05-26-pair-trading.html",
+      date: "",
+      icon: "bi-graph-up",
     },
     {
       title: "Crypto News Sentiment",
@@ -120,6 +111,8 @@
       icon: "bi-chat-square-text",
     },
   ];
+
+  const projects = [];
 
   const researchPapers = [
     {
